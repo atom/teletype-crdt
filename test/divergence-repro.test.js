9faafbc847c88bd019bb5d6cf77497dee052f7b9
Nathan Sobo <nathan@github.com>
2017-08-23 13:30:45 -0600

    WIP: Add failing test for divergent scenario
    The data behind this test was collected after a divergence occurred in
    the wild. This test doesn't actually demonstrate divergence due to only
    replaying remote operations, but it does fail a newly-added assertion.

---
+++ test/divergence-repro.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert')
+const Document = require('./helpers/document')
+const DocumentReplica = require('../lib/document-replica')
+const Peer = require('./helpers/peer')
+const Random = require('random-seed')
+
+const fs = require('fs')
+const path = require('path')
+
+suite('DocumentReplica', () => {
+  test.only('divergence', () => {
+    const hostOps = JSON.parse(fs.readFileSync(path.join(__dirname, 'host-ops.json'), 'utf8'))
+    const guestOps = JSON.parse(fs.readFileSync(path.join(__dirname, 'guest-ops.json'), 'utf8'))
+    const hostReplicaText = fs.readFileSync(path.join(__dirname, 'replica-text-host.txt'), 'utf8')
+    const guestReplicaText = fs.readFileSync(path.join(__dirname, 'replica-text-guest.txt'), 'utf8')
+
+    const host = new DocumentReplica(1)
+    const guest = new DocumentReplica(2)
+
+    for (const op of guestOps) {
+      guest.applyRemote(op)
+    }
+
+    for (const op of hostOps) {
+      host.applyRemote(op)
+    }
+
+    // console.log(guest.getText());
+    assert.equal(host.getText(), guest.getText())
+    // assert.equal(guest.getText(), hostReplicaText)
+    assert.equal(guestReplicaText, guest.getText())
+  })
+})
