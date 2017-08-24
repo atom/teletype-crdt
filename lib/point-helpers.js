atom/teletype-crdt

commit 0c0c656f0d14250f4a5652f23288702f734d7861

Add assertions to catch observed corruption earlier
Signed-off-by: Jason Rudolph <jasonrudolph@github.com>

lib/point-helpers.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 exports.ZERO_POINT = Object.freeze({row: 0, column: 0})
 
 exports.compare = function (a, b) {
@@ -55,6 +57,8 @@ exports.characterIndexForPosition = function (text, target) {
 
     index++
   }
+
+  assert(exports.compare(position, target) <= 0, 'Target position should not exceed the extent of the given text')
 
   return index
 }

lib/split-tree.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const SplayTree = require('./splay-tree')
-const {ZERO_POINT, compare, traverse, traversal, characterIndexForPosition} = require('./point-helpers')
+const {ZERO_POINT, compare, traverse, traversal, characterIndexForPosition, extentForText} = require('./point-helpers')
 
 module.exports =
 class SplitTree extends SplayTree {
@@ -77,11 +77,14 @@ class SplitTree extends SplayTree {
     const suffix = Object.assign({}, segment)
     suffix.text = segment.text.slice(splitIndex)
     suffix.extent = traversal(segment.extent, offset)
+    assert(compare(extentForText(suffix.text), suffix.extent) === 0, 'Suffix extent should match the actual extent of suffix text')
+
     suffix.opId = Object.assign({}, segment.opId)
     suffix.offset = traverse(suffix.offset, offset)
     suffix.deletions = new Set(suffix.deletions)
     segment.text = segment.text.slice(0, splitIndex)
     segment.extent = offset
+    assert(compare(extentForText(segment.text), segment.extent) === 0, 'Prefix extent should match the actual extent of prefix text')
     segment.nextSplit = suffix
 
     this.root = suffix

commit 5dbcfb6656d24535526bf3294fb50d94676ec166

Allocate a new position on each iteration of `characterIndexForPosition`
In trying to debug the divergence problem we have been experiencing, I
noticed that `characterIndexForPosition` exhibited a different behavior
when the debugger was on.

In particular, I was able to observe the assertion error introduced in
0c0c656 when I was not debugging the function. When adding `debugger`
statements or stepping through the source code, the assertion did not
fail. Since v8 disables optimizations when the debugger is on, I started
wondering if this bug was manifesting itself due to some logic error in
the optimizer.

This initially seemed very unlikely and led me to think there was some
other kind of race condition that was being prevented by adding delays
in the execution as I was stepping through the source code and
inspecting variables. However, the assertion stopped failing once this
commit was applied to my local checkout of the real-time package, and I
couldn't seem to reproduce the assertion errors anymore after that
(either with or without the debugger).

I still think this is a pretty mysterious issue, especially because our
randomized tests are not reproducing it at all. However, it's hard to
say whether v8 optimizations are kicking in while running tests, as
every test is wrapped in a try/catch in order to show failure messages.

lib/point-helpers.js
@@ -45,14 +45,13 @@ exports.extentForText = function (text) {
 }
 
 exports.characterIndexForPosition = function (text, target) {
-  const position = {row: 0, column: 0}
+  let position = {row: 0, column: 0}
   let index = 0
   while (exports.compare(position, target) < 0 && index <= text.length) {
     if (text[index] === '\n') {
-      position.row++
-      position.column = 0
+      position = {row: position.row + 1, column: 0}
     } else {
-      position.column++
+      position = {row: position.row, column: position.column + 1}
     }
 
     index++