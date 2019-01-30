508688fdcebb7b398638740b534fb92a648aae95
Antonio Scandurra <as-cii@github.com>
2019-01-30 14:15:39 +0100

    Fix vulnerabilities

--- script/electron-test-runner/main.js
+++ script/electron-test-runner/main.js
@@ -10,7 +10,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {nodeIntegration: true}
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
